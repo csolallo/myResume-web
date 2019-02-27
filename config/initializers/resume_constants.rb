f6f299031feb07cfd56adfc81ce05e8ed4a341df
Carlos Sola-Llonch <csolallo@gmail.com>
2019-02-26 22:46:19 -0800

    using threads for async calls in controller

---
+++ config/initializers/resume_constants.rb
@@ -0,0 +1,4 @@
+# Be sure to restart your server when you modify this file.
+
+Rails.application.config.resume_user = ENV['RESUME_USER']
+Rails.application.config.api_root = "https://resume.sola-llon.ch/api/v1"
